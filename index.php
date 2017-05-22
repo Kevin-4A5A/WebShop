eb8c15fb6ceee07a3b368d60782eff243014ea4c
Youssef-4a5a <430149@edu.rocmn.nl>
2017-05-22 11:02:53 +0200

    initial bestanden aangemaakt

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8"> 
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> 
+        <script type="text/javascript" src="scripting.js"></script>
+        <title>Multiversum - Index</title>
+    </head>
+    <header>
+        <div id="logo">
+            <img src="images/logo_header.png">
+        </div>
+        <div id="h1-title">
+            <h1>Multiversum</h1>
+        </div>
+        <div id="topmenu">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="prdocuten.php">Producten</a></li>
+            </ul>
+        </div>
+        <div id="shopping-cart">
+            <i class="fa fa-shopping-cart"></i>
+        </div>
+    </header>
+    <body>
+        
+    </body>
+    <footer>
+        
+    </footer>
+</html>
