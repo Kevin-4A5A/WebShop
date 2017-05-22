d3035318feaa7b283af2de4ef4004e143ca2319d
retal <retal@192.168.0.104>
2017-05-22 14:35:00 +0200

    product overzicht pagina v1

---
+++ producten.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8"> 
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Quicksand:400,700" rel="stylesheet"> 
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> 
+        <script type="text/javascript" src="scripting.js"></script>
+        <title>Multiversum - Producten</title>
+    </head>
+    <header>
+        <div id="logo">
+            <img src="images/logo_header.png">
+        </div>
+        <div id="topmenu">
+            <a class="links" href="index.php">Home</a>
+            <a class="links" href="prdocuten.php">Producten</a>
+            <a id="shopping-cart" href="winkelmandje.php"><i class="fa fa-shopping-cart"></i><div id="shopping-cart-counter">1</div></a>
+        </div>
+    </header>
+    <body>
+        <div class="row">
+        <div id="container">
+            <h1>Producten</h1>
+            <ul>
+                <li><div class="product-container"><img src="images/product.png"><div class="post-entry"><div class="post-title"><strong>Lorem ipsum</strong></div>Prijs: 500 euro<br><button>Bestellen</button></div></div></li>
+                <li><div class="product-container"><img src="images/product.png"><div class="post-entry"><div class="post-title"><strong>Lorem ipsum</strong></div>Prijs: 500 euro<br><button>Bestellen</button></div></div></li>
+                <li><div class="product-container"><img src="images/product.png"><div class="post-entry"><div class="post-title"><strong>Lorem ipsum</strong></div>Prijs: 500 euro<br><button>Bestellen</button></div></div></li>
+                <li><div class="product-container"><img src="images/product.png"><div class="post-entry"><div class="post-title"><strong>Lorem ipsum</strong></div>Prijs: 500 euro<br><button>Bestellen</button></div></div></li>
+                <li><div class="product-container"><img src="images/product.png"><div class="post-entry"><div class="post-title"><strong>Lorem ipsum</strong></div>Prijs: 500 euro<br><button>Bestellen</button></div></div></li>
+                <li><div class="product-container"><img src="images/product.png"><div class="post-entry"><div class="post-title"><strong>Lorem ipsum</strong></div>Prijs: 500 euro<br><button>Bestellen</button></div></div></li>
+                <li><div class="product-container"><img src="images/product.png"><div class="post-entry"><div class="post-title"><strong>Lorem ipsum</strong></div>Prijs: 500 euro<br><button>Bestellen</button></div></div></li>
+                <li><div class="product-container"><img src="images/product.png"><div class="post-entry"><div class="post-title"><strong>Lorem ipsum</strong></div>Prijs: 500 euro<br><button>Bestellen</button></div></div></li>
+                <li><div class="product-container"><img src="images/product.png"><div class="post-entry"><div class="post-title"><strong>Lorem ipsum</strong></div>Prijs: 500 euro<br><button>Bestellen</button></div></div></li>
+                <li><div class="product-container"><img src="images/product.png"><div class="post-entry"><div class="post-title"><strong>Lorem ipsum</strong></div>Prijs: 500 euro<br><button>Bestellen</button></div></div></li>
+            </ul>
+        </div>
+        </div>
+    </body>
+    <footer>
+
+    </footer>
+</html>
